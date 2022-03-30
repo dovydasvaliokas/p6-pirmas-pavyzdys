ca09730147334ac2079f64d5e35b552454eb0747
dovydasvaliokas <74957396+dovydasvaliokas@users.noreply.github.com>
2022-03-30 10:50:51 +0300

    Sukuriau nauja git projekta

---
+++ pirmas-pavyzdys/script.js
@@ -0,0 +1,35 @@
+// Pagrindinė funkcija (pagrindinė kodo dalis)
+window.onload = function() {
+    // Viršuje tiek pagrindinės funkcijos, tiek papildomų funkcijų susikuriame kintamuosius, kuriuos naudosime toje funkcijoje.
+    // Pirmam pavyzdyje pabandome susikurti x ir y ir apskaičiuoti jų sumą ir atimtį (nenaudojant papildomų funkcijų)
+    // Kuriame kintamąjį rašydami "let kintamojoPavadinimas;"
+    let x;
+    let y;
+
+    let suma;
+
+    // Norint priskirti reikšmę kintamajui/išsaugoti rezultatą kintamajame, tai rašome "kintamojoPavadinimas = reikšmė"
+    x = 5;
+    y = 10;
+
+
+    // Toliau, reikėtų susikurti sumos kintamąjį ir jame išsaugoti sumos rezultatą. Tačiau, kintamuosius JavaScript'e pagal "negriežtą" taisyklę reikia visada kurti viršuje.
+    suma = x + y;
+
+
+    // išvedimas į konsolę yra daromas console.log(kintamasis arba reikšmė ką norite išvesti);
+    console.log(suma);
+
+
+    // Jeigu norime išvesti į konsolę kelis kintamuosius arba kelias reikšmes, tai galime juos atskirti su pliuso ženklu skliausteliuose.
+    // Pavyzdžiui, jeigu norime išvesti "x reikšmė yra 5", tai darytume taip:
+    // Norint naudoti teksto reikšmę (o ne kintamąjį), tą tekstą reikia įrašyti tarp kabučių
+    console.log("X reikšmė yra " + x);
+    console.log("Y reikšmė yra " + y);
+    console.log("Suma tarp x ir y yra: " + suma);
+
+}
+
+
+
+// Papildomos funkcijos
